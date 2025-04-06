c8d9063a08e2fd34f7d6b639fd9ba1b81479235d
Architect-SCS <clash.warbone@gmail.com>
2025-04-06 01:41:13 -0400

    Create index.jsx

---
+++ neo_license_frontend/index.jsx
@@ -0,0 +1,11 @@
+// index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LicenseUnlock from './neo_license_frontend.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <LicenseUnlock />
+  </React.StrictMode>
+);
